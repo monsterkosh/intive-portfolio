ba23d219ac3e4c2f922ae477379ef688239d1428
Emiliano <mnnnkosh@gmail.com>
2021-11-24 01:11:13 -0300

    new data

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -3,8 +3,28 @@ import arch from '../assets/websites/arch.png';
 import bravo from '../assets/websites/bravo.png';
 import jeremy from '../assets/websites/jeremy.png';
 import blogplace from '../assets/websites/blogplace.png';
+import nico from '../assets/websites/nico.png';
+import sunnyside from '../assets/websites/sunnyside.png';
+import myownlanding from '../assets/websites/ownlanding.png';
+import exerc8 from '../assets/websites/exerc-8.png';
+import exerc7 from '../assets/websites/exerc7.png';
+import exerc51 from '../assets/websites/exerc51.png';
+import exerc52 from '../assets/websites/exerc52.png';
+import exerc53 from '../assets/websites/exerc53.png';
+import exerc54 from '../assets/websites/exerc54.png';
+import exerc55 from '../assets/websites/exerc55.png';
+import exerc4 from '../assets/websites/exerc4.png';
+import exerc3 from '../assets/websites/exerc3.png';
+import exerc2 from '../assets/websites/exerc2.png';
 
 const data = [
+  {
+    title: 'BlogPlace',
+    url: 'https://blogplace.netlify.app/',
+    github: 'https://github.com/monsterkosh/final-blog',
+    image: blogplace,
+    developer: 'Emiliano Franco',
+  },
   {
     title: 'The Planets',
     url: 'https://team-venus.netlify.app',
@@ -34,10 +54,115 @@ const data = [
     developer: 'Team Jeremy',
   },
   {
-    title: 'BlogPlace',
-    url: 'https://blogplace.netlify.app/',
-    github: 'https://github.com/monsterkosh/final-blog',
-    image: blogplace,
+    title: 'Nico',
+    url: 'https://nico-proyect.netlify.app/',
+    github:
+      'https://github.com/monsterkosh/training/tree/master/HTML%20%26%20CSS/Ejercicio%2010/Landing%201',
+    image: nico,
+    developer: 'Emiliano Franco',
+  },
+  {
+    title: 'SunnySide',
+    url: 'https://sunnyside-proyect.netlify.app/',
+    github:
+      'https://github.com/monsterkosh/training/tree/master/HTML%20%26%20CSS/Ejercicio%2011',
+    image: sunnyside,
+    developer: 'Emiliano Franco',
+  },
+  {
+    title: 'My own landing',
+    url: 'https://myownlanding-proyect.netlify.app/',
+    github:
+      'https://github.com/monsterkosh/training/tree/master/HTML%20%26%20CSS/Ejercicio%2010/Own%20Landing',
+    image: myownlanding,
+    developer: 'Emiliano Franco',
+  },
+  {
+    title: 'Exercise 8',
+    url: 'https://exerc-8.netlify.app/',
+    github:
+      'https://github.com/monsterkosh/training/tree/master/HTML%20%26%20CSS/Ejercicio%208',
+    image: exerc8,
+    developer: 'Emiliano Franco',
+  },
+  {
+    title: 'Exercise 7',
+    url: 'https://exerc-7.netlify.app/',
+    github:
+      'https://github.com/monsterkosh/training/tree/master/HTML%20%26%20CSS/Ejercicio%207',
+    image: exerc7,
+    developer: 'Emiliano Franco',
+  },
+  {
+    title: 'Exercise 7',
+    url: 'https://exerc-7.netlify.app/',
+    github:
+      'https://github.com/monsterkosh/training/tree/master/HTML%20%26%20CSS/Ejercicio%207',
+    image: exerc7,
+    developer: 'Emiliano Franco',
+  },
+  {
+    title: 'Exercise 5 - 1',
+    url: 'https://exerc-5-1.netlify.app/',
+    github:
+      'https://github.com/monsterkosh/training/tree/master/HTML%20%26%20CSS/Ejercicio%205/5.1',
+    image: exerc51,
+    developer: 'Emiliano Franco',
+  },
+  {
+    title: 'Exercise 5 - 2',
+    url: 'https://exerc-5-2.netlify.app/',
+    github:
+      'https://github.com/monsterkosh/training/tree/master/HTML%20%26%20CSS/Ejercicio%205/5.2',
+    image: exerc52,
+    developer: 'Emiliano Franco',
+  },
+  {
+    title: 'Exercise 5 - 3',
+    url: 'https://exerc-5-3.netlify.app/',
+    github:
+      'https://github.com/monsterkosh/training/tree/master/HTML%20%26%20CSS/Ejercicio%205/5.3',
+    image: exerc53,
+    developer: 'Emiliano Franco',
+  },
+  {
+    title: 'Exercise 5 - 4',
+    url: 'https://exerc-5-4.netlify.app/',
+    github:
+      'https://github.com/monsterkosh/training/tree/master/HTML%20%26%20CSS/Ejercicio%205/5.4',
+    image: exerc54,
+    developer: 'Emiliano Franco',
+  },
+  {
+    title: 'Exercise 5 - 5',
+    url: 'https://exerc-5-5.netlify.app/',
+    github:
+      'https://github.com/monsterkosh/training/tree/master/HTML%20%26%20CSS/Ejercicio%205/5.5',
+    image: exerc55,
+    developer: 'Emiliano Franco',
+  },
+  {
+    title: 'Exercise 4',
+    url: 'https://exerc-4.netlify.app/',
+    github:
+      'https://github.com/monsterkosh/training/tree/master/HTML%20%26%20CSS/Ejercicio%204',
+    image: exerc4,
+    developer: 'Emiliano Franco',
+  },
+  {
+    title: 'Exercise 3',
+    url: 'https://exerc-3.netlify.app/',
+    github:
+      'https://github.com/monsterkosh/training/tree/master/HTML%20%26%20CSS/Ejercicio%203',
+    image: exerc3,
+    developer: 'Emiliano Franco',
+  },
+  {
+    title: 'Exercise 2',
+    url: 'https://exerc-2.netlify.app/',
+    github:
+      'https://github.com/monsterkosh/training/tree/master/HTML%20%26%20CSS/Ejercicio%202',
+    image: exerc2,
     developer: 'Emiliano Franco',
   },
 ];
